59263d00733329822decdc04d863df5e9cebc83c
Aaron Key <aaronmikelkey@gmail.com>
2022-10-21 15:27:58 -0400

    Update nav css

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import '../assets/css/navbar.css'
 
 const Nav = (props) => {
 	//remove active classes from other items
@@ -24,55 +25,58 @@ const Nav = (props) => {
 	})
 
 	return (
-		<nav className="nav darkmode">
-			<ul>
-				<li
-					id="About"
-					className="active"
-					onClick={() => {
-						removeActive(), props.setCurrent('About')
-					}}
-				>
-					<a href="#About">
-						<i className="fas fa-home" />
-						<span>About</span>
-					</a>
-				</li>
-				<li
-					id="Portfolio"
-					onClick={() => {
-						removeActive(), props.setCurrent('Portfolio')
-					}}
-				>
-					<a href="#Portfolio">
-						<i className="fas fa-code-branch" />
-						<span>Portfolio</span>
-					</a>
-				</li>
-				<li
-					id="Contact"
-					onClick={() => {
-						removeActive(), props.setCurrent('Contact')
-					}}
-				>
-					<a href="#Contact">
-						<i className="fas fa-envelope" />
-						<span>Contact</span>
-					</a>
-				</li>
-				<li
-					id="Resume"
-					onClick={() => {
-						removeActive(), props.setCurrent('Resume')
-					}}
-				>
-					<a href="#Resume">
-						<i className="fas fa-info-circle" />
-						<span>Resume</span>
-					</a>
-				</li>
-			</ul>
-		</nav>
+		<header>
+			<span className="header-span">Aaron Key</span>
+			<nav className="nav">
+				<ul>
+					<li
+						id="About"
+						className="active"
+						onClick={() => {
+							removeActive(), props.setCurrent('About')
+						}}
+					>
+						<a href="#About">
+							<i className="fas fa-home" />
+							<span>About</span>
+						</a>
+					</li>
+					<li
+						id="Portfolio"
+						onClick={() => {
+							removeActive(), props.setCurrent('Portfolio')
+						}}
+					>
+						<a href="#Portfolio">
+							<i className="fas fa-code-branch" />
+							<span>Portfolio</span>
+						</a>
+					</li>
+					<li
+						id="Contact"
+						onClick={() => {
+							removeActive(), props.setCurrent('Contact')
+						}}
+					>
+						<a href="#Contact">
+							<i className="fas fa-envelope" />
+							<span>Contact</span>
+						</a>
+					</li>
+					<li
+						id="Resume"
+						onClick={() => {
+							removeActive(), props.setCurrent('Resume')
+						}}
+					>
+						<a href="#Resume">
+							<i className="fas fa-info-circle" />
+							<span>Resume</span>
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
 	)
 }
 
