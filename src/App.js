c63bccad6f985646865e45a5bf60fa3053da1a59
Aaron Key <aaronmikelkey@gmail.com>
2022-10-22 00:13:12 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import Nav from './components/Nav'
+import About from './components/About'
+import Contact from './components/Contact'
+import Portfolio from './components/Portfolio'
+import Resume from './components/Resume'
+import Footer from './components/Footer'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/brands'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	const [currentTab, setCurrentTab] = useState('About')
+	let currentPage = <About />
+	switch (currentTab) {
+		case 'About':
+			currentPage = <About />
+			break
+		case 'Contact':
+			currentPage = <Contact />
+			break
+		case 'Portfolio':
+			currentPage = <Portfolio />
+			break
+		case 'Resume':
+			currentPage = <Resume />
+			break
+		default:
+			currentPage = <About />
+			break
+	}
+	return (
+		<div className="App">
+			<Nav current={currentTab} setCurrent={setCurrentTab} />
+			<main>{currentPage}</main>
+			<Footer />
+		</div>
+	)
 }
 
-export default App;
+export default App
